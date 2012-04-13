1d3fe144f1f71feb1fa050c078de210d834eace9
Cairnarvon <cairnarvon@gmail.com>
2012-04-13 02:06:52 +0200

    Syntax errors now raise SexpCodeException.

--- sexpcode.c
+++ sexpcode.c
@@ -1,38 +1,63 @@
 #include <Python.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "sexpcode/sexpcode.h"
 #include "sexpcode/html.h"
 
 sexpcode_func *(*get_func)(char*);
 
+static PyObject *SexpCodeException;
+
 static PyObject*
 sexpcode_sexpcode(PyObject *self, PyObject *args)
 {
     const char *input, *output;
+    char err[256];
+    FILE *stderr_tmp = stderr;
 
     if (!PyArg_ParseTuple(args, "s", &input))
         return NULL;
 
     get_func = &html_get_func;
+    stderr = tmpfile();
     output = sexpcode(input);
 
-    if (output != NULL)
+    if (output != NULL) {
+        fclose(stderr);
+        stderr = stderr_tmp;
+
         return Py_BuildValue("s", output);
-    else {
-        Py_INCREF(Py_None);
-        return Py_None;
+    } else {
+        int i;
+
+        rewind(stderr);
+        i = fread(err, 1, 255, stderr);
+        err[i - 1] = 0;
+        fclose(stderr);
+        stderr = stderr_tmp;
+
+        PyErr_SetString(SexpCodeException, err);
+        return NULL;
     }
 }
 
 static PyMethodDef sexpcode_methods[] = {
     {"sexpcode", sexpcode_sexpcode, METH_VARARGS,
-     "Converts SexpCode to HTML. Returns None on error."},
+     "Converts SexpCode to HTML. Raises SexpCodeException on error."},
     {NULL, NULL, 0, NULL}
 };
 
 PyMODINIT_FUNC
 initsexpcode(void)
 {
-        (void)Py_InitModule("sexpcode", sexpcode_methods);
+    PyObject *m;
+
+    m = Py_InitModule("sexpcode", sexpcode_methods);
+    if (m == NULL)
+        return;
+
+    SexpCodeException = PyErr_NewException("sexpcode.SexpCodeException", NULL, NULL);
+    Py_INCREF(SexpCodeException);
+    PyModule_AddObject(m, "SexpCodeException", SexpCodeException);
 }
